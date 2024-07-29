4dcd0bce7a3649684e0f4ce0a01d34bd0d7d0e4f
nayakiselchik <150137682+nayakiselchik@users.noreply.github.com>
2024-07-29 21:11:26 +0300

    sliding check

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.slide-in');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.1 });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
